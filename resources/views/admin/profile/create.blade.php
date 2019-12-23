5d53d11496702f7670693565a0dac30ff6d9f1d1
24_Oishi_Yukio <yuttyo021@gmail.com>
2019-12-23 07:30:10 +0000

    2019.12.23

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=devise-width, initial-scale=1">
+
+        <title>Myプロフィール</title>
+    </head>
+    <body>
+        
+        {{-- layouts/profile.blade.phpを読み込む --}}
+        @extends('layouts.profile')
+        
+        {{-- profile.blade.phpの@yield('title')に'Myプロフィール'を埋め込む --}}
+        @section('title', 'Myプロフィール')
+        
+        {{-- profile.blade.phpの@yield('content')に以下を埋め込む --}}
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>Myプロフィール</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
+        </body>
+</html>
