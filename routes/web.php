25edc0c3817c57717e0ec52ec7bfd0b3dcb0b720
24_Oishi_Yukio <yuttyo021@gmail.com>
2020-01-02 09:20:44 +0000

    テキスト16

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,14 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     
     Route::get('news/create', 'Admin\NewsController@add');
 
-    Route::post('news/create', 'Admin\NewsController@create'); # 追記
-// 以下課題4
+    Route::post('news/create', 'Admin\NewsController@create');
+    
+    Route::get('news', 'Admin\NewsController@index'); 
+
+    Route::get('news/edit', 'Admin\NewsController@edit'); 
+
+    Route::post('news/edit', 'Admin\NewsController@update'); 
+
     Route::get('profile/create', 'Admin\ProfileController@add');
     
     Route::post('profile/create', 'Admin\ProfileController@create');
