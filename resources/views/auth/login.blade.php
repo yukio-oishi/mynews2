6019529b05b37b40c0a3df31ea6d490f55c08917
24_Oishi_Yukio <yuttyo021@gmail.com>
2020-01-14 09:15:36 +0000

    2020.1.14

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,6 +59,13 @@
                                     </button>
                                 </div>
                             </div>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('messages.NewRegister') }}
+                                    </button>
+                                </div>
+                            </div>
                         </form>
                     </div>
                 </div>
